071aa053ab4429e70b575cb73ddf6f163f10021b
humay qasimova <hqasimova472@gmail.com>
2024-04-05 20:32:00 +0000

    weather

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 "use strict";
 $(document).ready(function () {
 
+
+
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = ``
+            async function myweather() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                $('#city').html(data.name);
+                $('#temp').html(data.main.temp);
+                $('#feels').html(data.main.feels_like);
+                $('#myweather').val("");
+                console.log(data)
+            }
+            myweather()
+        }
+    });
+
 });
