4ea7d9907d9a0bbc03fb2f54ad83c8bae2787135
humay qasimova <hqasimova472@gmail.com>
2024-04-06 15:10:30 +0000

    weather

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = ``
+            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${$('#myweather').val()}&appid=372ff2b57195903942fc8cc19158ad81`
             async function myweather() {
                 let responce = await fetch(url);
                 let data = await responce.json();
@@ -17,6 +17,21 @@ $(document).ready(function () {
             }
             myweather()
         }
+    });
+
+    $('#btn').click(function (e) {
+        e.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${$('#myweather').val()}&appid=372ff2b57195903942fc8cc19158ad81`
+        async function myweather() {
+            let responce = await fetch(url);
+            let data = await responce.json();
+            $('#city').html(data.name);
+            $('#temp').html(data.main.temp);
+            $('#feels').html(data.main.feels_like);
+            $('#myweather').val("");
+            console.log(data)
+        }
+        myweather()
     });
 
 });
