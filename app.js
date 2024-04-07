9b71952c8e948c08a8fc9f12108f34c487aa3375
humay qasimova <hqasimova472@gmail.com>
2024-04-07 15:26:48 +0000

    weather

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${$('#myweather').val()}&appid=372ff2b57195903942fc8cc19158ad81`
-            async function myweather() {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$('#myWeather').val()}&appid=984074dd5258590c1f3773e4752fa253&units=metric`;
+            async function myWeather() {
                 let responce = await fetch(url);
                 let data = await responce.json();
                 $('#city').html(data.name);
                 $('#temp').html(data.main.temp);
                 $('#feels').html(data.main.feels_like);
-                $('#myweather').val("");
+                $('#myWeather').val("");
                 console.log(data)
             }
-            myweather()
+            myWeather()
         }
     });
 
     $('#btn').click(function (e) {
         e.preventDefault();
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${$('#myweather').val()}&appid=372ff2b57195903942fc8cc19158ad81`
-        async function myweather() {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$('#myWeather').val()}&appid=984074dd5258590c1f3773e4752fa253&units=metric`;
+        async function myWeather() {
             let responce = await fetch(url);
             let data = await responce.json();
             $('#city').html(data.name);
             $('#temp').html(data.main.temp);
             $('#feels').html(data.main.feels_like);
-            $('#myweather').val("");
+            $('#myWeather').val("");
             console.log(data)
         }
-        myweather()
+        myWeather()
     });
 
 });
